bef21e74b7b1bcc11593e665ca4d95fd660c8355
richarddedor <41088656+richarddedor@users.noreply.github.com>
2018-10-21 17:41:38 -0500

    re-setting mail code

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,3 +1,7 @@
+Mail php
+
+///
+
 <?php
 // Check for empty fields
 if(empty($_POST['name'])  		||
@@ -15,4 +19,12 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
+// Create the email and send the message
+$to = 'dusty.juhl@trestletrailtech.com.test-google-a.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: no-reply@trestletrailtech.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
 ?>
